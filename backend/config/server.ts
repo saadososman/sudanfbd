b8baba850c4df4bada5c2711ccc0c08b37723dcf
Saad Madani <saadmadani54@gmail.com>
2026-05-26 00:04:45 +0300

    fix railway config as code

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,13 +1,35 @@
 export default ({ env }) => ({
+
   host: env('HOST', '0.0.0.0'),
 
   port: env.int('PORT', 1337),
+
+  url: env('PUBLIC_URL', ''),
 
   app: {
     keys: env.array('APP_KEYS'),
   },
 
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+    apiToken: {
+      salt: env('API_TOKEN_SALT'),
+    },
+    transfer: {
+      token: {
+        salt: env('TRANSFER_TOKEN_SALT'),
+      },
+    },
+  },
+
+  jwt: {
+    secret: env('JWT_SECRET'),
+  },
+
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+
 });
