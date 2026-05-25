dbbf93410f9d414d13db71858f1b9bf43b199ede
Saad Madani <saadmadani54@gmail.com>
2026-05-25 23:14:26 +0300

    fix server config

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,13 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+
+  port: env.int('PORT', 1337),
+
   app: {
-    keys: env.array("APP_KEYS", ["local-key-1", "local-key-2"])
-  }
+    keys: env.array('APP_KEYS'),
+  },
+
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
